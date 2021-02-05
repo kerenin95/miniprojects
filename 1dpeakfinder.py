67c476731b34c445c6ba48c758041b7d955daf95
ben weber <weberbencs@gmail.com>
2021-02-05 15:58:28 -0500

    optimal 1d peak finder

--- 1dpeakfinder.py
+++ 1dpeakfinder.py
@@ -1,10 +1,21 @@
 '''algorithm to find all 1d peak in an unsorted list if one exists.'''
 
-l = [1,4,7,0,2,4,7,9,6,4,3,0,1,4,7,4,2,9]
+l = [1,4,7,0,2,4,7,9,8,4,7,9,1,4,7,4,2,9]
 
 def peakfind(l):
-    for i in l:
-        if (l[i] > l[i-1]) and (l[i] > l[i+1]): 
-                return "peak is {}".format(l[i])
+    midpoint = (len(l) // 2) # find the midpoint value
+    
+    if l[midpoint] < l[midpoint + 1]: # if the right side is greater scan the right side for a peak
+        for i in l[midpoint: -1]: 
+            if l[i] > l[i+1]: # compare current value with neighbors on right side
+                return "the peak on the right is {}".format(l[i])
+    
+    elif l[midpoint] < l[midpoint - 1]: # if the left side of the midpoint is greater 
+        for j in l[0: midpoint]: # scan the left side of midpoint for a peak
+            if l[j] > l[j-1]: # compare current value with item on the left
+                return "the peak on the left is {}".format(l[j])
+    
+    else: # if the midpoint is a peak already
+        return "the peak is middle value({})".format(l[midpoint])        
                 
 print(peakfind(l)) 
